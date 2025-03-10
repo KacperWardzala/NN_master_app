b1c23cb3cb571c7965fa1e957ed8d195780475ba
KacperWardzala <143707644+KacperWardzala@users.noreply.github.com>
2025-03-10 21:18:26 +0100

    Update web_guess.py
    English version and GitHub link aded

--- web_guess.py
+++ web_guess.py
@@ -37,14 +37,43 @@ def preprocess_sequence(seq, tokenizer, max_length=500):
     return padded_seq
 
 # Interfejs użytkownika w Streamlit
-st.title("Klasyfikacja bakterii na podstawie sekwencji RNA")
-st.write("Wprowadź sekwencję 16S rRNA, aby przewidzieć przynależność bakteryjną.")
+st.set_page_config(page_title="RNA Bacteria Classification", layout="centered")
 
-user_sequence = st.text_input("Wprowadź sekwencję RNA:", "")
+# Wybór języka
+language = st.selectbox("Wybierz język / Select language", ["Polski", "English"])
 
-if st.button("Przeanalizuj sekwencję"):
+# Teksty dla obu języków
+texts = {
+    "Polski": {
+        "title": "Klasyfikacja bakterii na podstawie sekwencji RNA",
+        "description": "Wprowadź sekwencję 16S rRNA, aby przewidzieć przynależność bakteryjną.",
+        "input_label": "Wprowadź sekwencję RNA:",
+        "analyze_button": "Przeanalizuj sekwencję",
+        "warning": "Wprowadź sekwencję",
+        "result_label": "**Przewidywana bakteria:**",
+        "probability_label": "**Prawdopodobieństwo:**",
+        "repo_link": "Mój kod źródłowy: [GitHub Repo](https://github.com/KacperWardzala/NN_master_app)"
+    },
+    "English": {
+        "title": "Bacteria Classification Based on RNA Sequence",
+        "description": "Enter a 16S rRNA sequence to predict bacterial affiliation.",
+        "input_label": "Enter RNA sequence:",
+        "analyze_button": "Analyze sequence",
+        "warning": "Please enter a sequence",
+        "result_label": "**Predicted bacteria:**",
+        "probability_label": "**Probability:**",
+        "repo_link": "My source code: [GitHub Repo](https://github.com/KacperWardzala/NN_master_app)"
+    }
+}
+
+st.title(texts[language]["title"])
+st.write(texts[language]["description"])
+
+user_sequence = st.text_input(texts[language]["input_label"], "")
+
+if st.button(texts[language]["analyze_button"]):
     if not user_sequence.strip():
-        st.warning("Wprowadź sekwencję")
+        st.warning(texts[language]["warning"])
     else:
         processed_sequence = preprocess_sequence(user_sequence, tokenizer)
         prediction = model.predict(processed_sequence)
@@ -52,5 +81,8 @@ if st.button("Przeanalizuj sekwencję"):
         predicted_bacteria = reverse_label_map[predicted_label_index]
         predicted_probability = prediction[0][predicted_label_index]
 
-        st.success(f"**Przewidywana bakteria:** {predicted_bacteria}")
-        st.write(f"**Prawdopodobieństwo:** {predicted_probability:.4f}")
+        st.success(f"{texts[language]['result_label']} {predicted_bacteria}")
+        st.write(f"{texts[language]['probability_label']} {predicted_probability:.4f}")
+
+# Link do repozytorium
+st.markdown(texts[language]["repo_link"])
